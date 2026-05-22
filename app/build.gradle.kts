f3b03d105fbe9f8df8a6ec886583e9f04d70be69
remerer <remerer@gmail.com>
2026-05-22 19:40:41 +0900

    Release 1.2.3 classic background settings

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.remerer.mobiledeck"
         minSdk = 24
         targetSdk = 36
-        versionCode = 14
-        versionName = "1.2.2"
+        versionCode = 15
+        versionName = "1.2.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
